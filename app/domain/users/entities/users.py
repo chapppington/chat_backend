19693c89b1c16d0e878412f51ac97abf5cccffaa
chapppington <stas.mega.96@bk.ru>
2026-01-09 18:52:15 +0700

    (feat) added base infra for users

--- app/domain/users/entities/users.py
+++ app/domain/users/entities/users.py
@@ -1,4 +1,6 @@
 from dataclasses import dataclass
+from datetime import datetime
+from typing import Optional
 
 from domain.base.entity import BaseEntity
 from domain.users.value_objects import (
@@ -12,3 +14,4 @@ class UserEntity(BaseEntity):
     email: EmailValueObject
     hashed_password: str
     name: UserNameValueObject
+    last_online_at: Optional[datetime] = None
